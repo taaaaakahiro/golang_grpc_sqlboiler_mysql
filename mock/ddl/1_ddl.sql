ab5b1c7296f3af6b8f6b52b297caa5f3798855de
takahiro <takapi854@gmail.com>
2022-11-27 09:54:48 +0900

    push

--- mock/ddl/1_ddl.sql
+++ mock/ddl/1_ddl.sql
@@ -4,9 +4,3 @@ CREATE TABLE `example`.`m_users` (
     age INT(3) NOT NULL,
     PRIMARY KEY (id)
 );
-
-CREATE TABLE `example`.`m_items` (
-	id INT(11) AUTO_INCREMENT NOT NULL, 
-	name VARCHAR(10) NOT NULL,
-    PRIMARY KEY (id)
-);
