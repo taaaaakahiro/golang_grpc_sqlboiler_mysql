5ffbf519cefd17aa3b530f112bac1b8da49c8f05
takahiro <takapi854@gmail.com>
2022-11-27 10:37:43 +0900

    grpc client

--- Makefile
+++ Makefile
@@ -4,3 +4,5 @@ run:
 sqlboiler:
 	sqlboiler mysql -c sqlboiler.toml -o ./pkg/models --no-tests
 	
+client:
+	go run ./external/grpc/main.go
--- external/grpc/main.go
+++ external/grpc/main.go
@@ -1,1 +1,63 @@
-package grpc
+package main
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	grpcpb "golang-grpc-sqlboiler-mysql/pkg/grpc"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+var (
+	scanner *bufio.Scanner
+	client  grpcpb.UserServiceClient
+)
+
+func main() {
+	fmt.Println("start gRPC Client.")
+
+	// 1. 標準入力から文字列を受け取るスキャナを用意
+	scanner = bufio.NewScanner(os.Stdin)
+
+	// 2. gRPCサーバーとのコネクションを確立
+	address := "localhost:8080"
+	conn, err := grpc.Dial(
+		address,
+
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithBlock(),
+	)
+	if err != nil {
+		log.Fatal("Connection failed.")
+		return
+	}
+	defer conn.Close()
+
+	// 3. gRPCクライアントを生成
+	client = grpcpb.NewUserServiceClient(conn)
+	User()
+}
+
+func User() {
+	fmt.Println("Please enter num.")
+	scanner.Scan()
+	text := scanner.Text()
+	id, _ := strconv.Atoi(text)
+
+	req := &grpcpb.UserRequest{
+
+		Id: int32(id),
+	}
+	res, err := client.User(context.Background(), req)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(res.Name)
+	}
+}
